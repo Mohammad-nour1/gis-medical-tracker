52d2e9e89946b7572b27937f3e6311d4b4d9f0a8
Mohammad-nour1 <nour88255@gmail.com>
2026-08-05 10:04:26 +0300

    Make facility name normalization update-only for safer Supabase runs.

--- infrastructure/database/migrations/007_normalize_facility_names.sql
+++ infrastructure/database/migrations/007_normalize_facility_names.sql
@@ -2,16 +2,3 @@ UPDATE facilities SET name = 'Damascus Central Hospital' WHERE name IN ('مشف�
 UPDATE facilities SET name = 'Aleppo University Hospital' WHERE name IN ('مشفى حلب الجامعي');
 UPDATE facilities SET name = 'Latakia National Hospital' WHERE name IN ('مشفى اللاذقية الوطني');
 UPDATE facilities SET name = 'Bab Tuma Clinic' WHERE name IN ('مستوصف باب توما');
-
-DELETE FROM facilities a
-USING facilities b
-WHERE a.id <> b.id
-  AND a.name = b.name
-  AND a.governorate = b.governorate
-  AND a.name IN (
-    'Damascus Central Hospital',
-    'Aleppo University Hospital',
-    'Latakia National Hospital',
-    'Bab Tuma Clinic'
-  )
-  AND a.id::text > b.id::text;
